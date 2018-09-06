ab829c0df08abee6e4f6e464d9d81f042cf36ea9
unknown <saherelgendy1990@gmail.com>
2018-09-06 23:37:40 +0300

    initial

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import Deck from './Deck';
+import './App.css';
+
+class App extends Component {
+  initialIcons = [];
+ 
+  render() {
+    return (
+     <Deck />
+    );
+  }
+}
+
+export default App;
---
+++ src/Deck.js
@@ -0,0 +1,32 @@
+import React, { Component } from 'react';
+
+import Card from './Card';
+
+class Deck extends Component {
+  render() {
+  	return(
+  	  <div className="deck">
+        <Card />
+         <Card />
+ <Card />
+  <Card />
+   <Card />
+    <Card />
+     <Card />
+      <Card />
+       <Card />
+        <Card />
+         <Card />
+          <Card />
+           <Card />
+            <Card />
+             <Card />
+              <Card />
+
+
+  	  </div>
+  	);
+  }
+}
+
+export default Deck;
