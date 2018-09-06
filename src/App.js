33ca9df4ea8c4b987962f7baba50a4e57163898d
unknown <saherelgendy1990@gmail.com>
2018-09-07 02:43:11 +0300

    creating shuffle cards function

--- src/App.js
+++ src/App.js
@@ -3,11 +3,29 @@ import Deck from './Deck';
 import './App.css';
 
 class App extends Component {
-  initialIcons = [];
- 
+
+  state = {
+    icons: ['fa fa-diamond', 'fa fa-bicycle', 'fa fa-bolt', 'fa fa-paper-plane-o',
+            'fa fa-anchor', 'fa fa-cube', 'fa fa-leaf', 'fa fa-bomb'],
+  }
+
+  shuffleCards = (arr) => {	
+
+    let shuffledCards = [];
+    let randomIcon;
+
+    while(shuffledCards.length < arr.length){
+      randomIcon = arr[Math.floor(Math.random() * arr.length)];
+      if(!shuffledCards.includes(randomIcon)) shuffledCards.push(randomIcon);
+    }
+     return shuffledCards;
+  }
+  
+
   render() {
+  	const { icons } = this.state;
     return (
-     <Deck />
+     <Deck icons={ this.shuffleCards(icons).concat(this.shuffleCards(icons)) }/>
     );
   }
 }
--- src/Deck.js
+++ src/Deck.js
@@ -4,26 +4,14 @@ import Card from './Card';
 
 class Deck extends Component {
   render() {
+    console.log(this.props.icons)
   	return(
   	  <div className="deck">
-        <Card />
-         <Card />
- <Card />
-  <Card />
-   <Card />
-    <Card />
-     <Card />
-      <Card />
-       <Card />
-        <Card />
-         <Card />
-          <Card />
-           <Card />
-            <Card />
-             <Card />
-              <Card />
-
-
+          {this.props.icons.map((icon,i) => {
+            return(
+              <div key={i} className="card"><i className={icon} /></div>
+            )  
+          })}   
   	  </div>
   	);
   }
